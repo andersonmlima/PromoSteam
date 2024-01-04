bb4c053d96e4e9955fec48f6f744888cfd680c0e
Anderson Moura <andersonmlima2@gmail.com>
2024-01-04 10:29:28 -0300

    Ajustes viewcode e arquitetura inicial da LoginScreen

---
+++ PromoSteam/PromoSteam/Login/LoginViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PromoSteam
+//
+//  Created by Anderson Moura on 03/01/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PromoSteam/PromoSteam/Login/Screen/LoginScreen.swift
@@ -0,0 +1,13 @@
+//
+//  LoginScreen.swift
+//  PromoSteam
+//
+//  Created by Anderson Moura on 04/01/24.
+//
+
+import UIKit
+
+class LoginScreen: UIView {
+    
+
+}
