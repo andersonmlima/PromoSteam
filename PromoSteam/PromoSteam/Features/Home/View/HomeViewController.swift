08ba162b32c7145d40e108bafb1614ef7b567132
Anderson Moura <andersonmlima2@gmail.com>
2024-01-11 20:44:01 -0300

    Implementando a API CheapShark API

--- PromoSteam/PromoSteam/Features/Home/View/HomeViewController.swift
+++ PromoSteam/PromoSteam/Features/Home/View/HomeViewController.swift
@@ -11,6 +11,45 @@ class HomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchCheapSharkDeals()
         
     }
-}
+    
+    func fetchCheapSharkDeals() {
+            // Substitua "YOUR_API_KEY" pela sua chave de API
+            let apiKey = "https://api.postman.com/collections/31016053-6a62b2e6-2ce1-4a1b-8a99-4fe5b50963f3?access_key=PMAT-01HKXDSBJHFHZMJWP53EPEC7QZ"
+            let apiURL = "https://api.cheapshark.com/1.0/deals?storeID=1&sortBy=Deal%20Rating&pageSize=10&desc=desc&onSale=1"
+
+            guard let url = URL(string: apiURL) else {
+                print("URL inválida")
+                return
+            }
+
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            request.addValue(apiKey, forHTTPHeaderField: "X-CheapShark-Token")
+
+            let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+                if let error = error {
+                    print("Erro na requisição: \(error.localizedDescription)")
+                    return
+                }
+
+                guard let data = data else {
+                    print("Dados não encontrados")
+                    return
+                }
+
+                do {
+                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                        print(json)
+                        // Aqui você pode processar os dados JSON conforme necessário
+                    }
+                } catch {
+                    print("Erro ao converter os dados JSON: \(error.localizedDescription)")
+                }
+            }
+
+            task.resume()
+        }
+    }
--- PromoSteam/PromoSteam/Features/Login/View/LoginViewController.swift
+++ PromoSteam/PromoSteam/Features/Login/View/LoginViewController.swift
@@ -38,7 +38,7 @@ class LoginViewController: UIViewController {
 
         // Autenticação com o Firebase
         Auth.auth().signIn(withEmail: email, password: password) { [weak self] (authResult, error) in
-            guard let strongSelf = self else {
+            guard self != nil else {
                 return
             }
 
